96f0d48f782ef3d448899c6ca7c9f9c92c47164c
Leandro Ungari <leandroungari@gmail.com>
2019-10-23 08:27:14 -0300

    feat: data area clickable

---
+++ src/components/DataArea/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {
+  Container
+} from '../../components';
+
+const Box = styled(Container)`
+  border-top: 1px solid #dedede;
+
+`;
+
+const Title = styled.h2`
+  font-size: 20px;
+  cursor: ${({onClick}) => onClick ? 'pointer' : 'auto'};
+  &:hover {
+    text-decoration: ${({onClick}) => onClick ? 'underline' : 'none'};
+  }
+`;
+
+export default function DataArea({
+  children, 
+  title,
+  onClick = undefined
+}) {
+  return (
+    <Box
+      padding="20px 20px"
+    >
+      <Title
+        onClick={onClick}
+      >
+        {title}
+      </Title>
+      {children}
+    </Box>
+  );
+}
