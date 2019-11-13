2a84d825f54e0a10b21e98a966ddef5c495bedf9
Leandro Ungari <leandroungari@gmail.com>
2019-11-12 21:02:24 -0300

    fix: import of topbar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 
 import Header from './Header';
-import TopBar from './TopBar';
 import HistoryMetrics from './HistoryMetrics';
 import Container from './Container';
 import TitlePage from './TitlePage';
@@ -9,7 +8,6 @@ import TitlePage from './TitlePage';
 export {
   Container,
   Header,
-  TopBar,
   HistoryMetrics,
   TitlePage
 }
