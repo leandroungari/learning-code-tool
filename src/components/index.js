82413c1495928867d90c25044b2796b2188b7d5f
Leandro Ungari <leandroungari@gmail.com>
2019-11-26 15:17:29 -0300

    feat: creating line plot component

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Header from './Header';
 import HistoryMetrics from './HistoryMetrics';
 import TitlePage from './TitlePage';
 import ListCommit from './ListCommit';
+import LinePlot from "./LinePlot";
 
 
 export {
   Header,
   HistoryMetrics,
   TitlePage,
-  ListCommit
+  ListCommit,
+  LinePlot
 }
